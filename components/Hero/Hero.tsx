7c2aa3264c31cc9a3ca5d02bb3f6840c1a9df9fd
CarnkyGus <jirateep.loyma02@gmail.com>
2022-06-22 13:38:42 +0700

    Add: taskbar

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -7,7 +7,15 @@ const Hero = () => {
         <img src="images/bg.jpeg" alt="" className=' relative top-0 left-[-1px]' />
       </div>
       <div className='flex justify-center items-center'>
-        <img src="images/Hero.png" alt="" className='  absolute top-32 w-[20%] md:w-[15%] md:top-[12rem]'/>
+        <img src="images/Hero.png" alt="" className='  absolute top-32 w-[20%] md:w-[15%] md:top-[12rem]' />
+      </div>
+      <div className=' bg-black h-16'>
+        <div className='flex justify-between items-center text-white px-14 py-5'>
+          <div className='font-[MinimalBold] text-xl'>
+            <p>Fast</p>
+            <p></p>
+          </div>
+        </div>
       </div>
     </>
   )
