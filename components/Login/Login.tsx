ebb532f6f23542e3f3b61ebd913df4eff05f018c
CarnkyGus <jirateep.loyma02@gmail.com>
2022-06-27 09:37:49 +0700

    Update: Add button and styles

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const LoginComponent = () => {
   return (
     <div className="bg-gradient-to-r from-violet-500 to-fuchsia-500 h-screen flex justify-center items-center p-4">
@@ -18,6 +20,25 @@ const LoginComponent = () => {
             type="password"
             className="w-full py-2 bg-gray-100 text-gray-500 px-1 rounded-md outline-none mb-2"
           />
+
+          <input type="checkbox" className="py-2" />
+          <label className="font-[Minimal] text-md px-2  ">Remember me? </label>
+          <button
+            type="submit"
+            className="px-6 py-3 rounded-lg hover:bg-gradient-to-r from-red-501 to-yellow-400  transition-all font-[Minimal] text-xl duration-500 bg-gray-300 mx-[6.5rem] mt-2"
+          >
+            {" "}
+            Login{" "}
+          </button>
+          <div className="font-[Minimal] text-md pt-2 mx-12  ">
+            {" "}
+            Don't Have an Account?{" "}
+            <span className="ml-1 font-[MinimalBold]">
+              <Link href="/register">
+                <a>Register</a>
+              </Link>
+            </span>
+          </div>
         </div>
       </form>
     </div>
