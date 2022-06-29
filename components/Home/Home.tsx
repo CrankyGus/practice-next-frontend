8ae32d4870f88bc6b62b89d0833d8b5c76ca9c88
CarnkyGus <jirateep.loyma02@gmail.com>
2022-06-29 11:36:18 +0700

    Update: HomeComponents

---
+++ components/Home/Home.tsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <div className="h-[200px] text-white bg-black flex justify-center items-center">
+      This is a HomePage for guests
+    </div>
+
+  );
+};
+
+export default HomePage;
